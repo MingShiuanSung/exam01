e37dde4236a82d85ab914bf9aad94daf32514f88
Steven Sung <steven10400211@gmail.com>
2020-04-01 17:22:35 +0800

    Add change

--- main.cpp
+++ main.cpp
@@ -2,14 +2,25 @@
 
 #include "uLCD_4DGL.h"
 
+// LED = D7
 
+// uLCD
 uLCD_4DGL uLCD(D1, D0, D2);
+
+// PWM
+PwmOut PWM1(D0);
+AnalogIn Ain(A0);
+
+// data to pc
+Serial pc( USBTX, USBRX );
+AnalogIn Ain(A0);
+float ADCdata;
 
 
 int main()
 
 {
-
+    // uLCD
       uLCD.printf("\n107061139\n"); //Default Green on black text
 
       uLCD.line(10 , 20, 10, 50, 0xFFFFFF);
@@ -17,6 +28,25 @@ int main()
       uLCD.line(10 , 20, 40, 20, 0xFFFFFF);
       uLCD.line(10 , 50, 40, 50, 0xFFFFFF);
 
-      wait(30);
+    // PWM
+    PWM1.period(0.001f);      // 4 second period
+
+    while (1) {
+        for (float i = 0.0f; i <= 1.0f; i += 0.1f) {
+            PWM1.write(i);      // 50% duty cycle, relative to period
+            ADCdata = PWM1;
+            pc.printf("%1.3f\r\n", ADCdata);
+            wait(0.1);
+        }
+
+        for (float i = 1.0f; i >= 0.0f; i -= 0.1f) {
+            PWM1.write(i);      // 50% duty cycle, relative to period
+            ADCdata = PWM1;
+            pc.printf("%1.3f\r\n", ADCdata);
+            wait(0.1);
+        }
+    }
+
+     
 
 }
